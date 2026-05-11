87bf504ab8546d0d1c8d9c6b85364b830db12997
Raihan Ali <trueraihan@gmail.com>
2026-05-11 13:19:03 +0600

    feat(users): add user module with create, update, and retrieval functionalities
    - Implemented CreateUserDto and UpdateUserDto for user data validation.
    - Added UserDto and PublicUserDto for user response formatting.
    - Created UsersService to handle user creation, retrieval, and updates.
    - Integrated global validation pipe in main application.
    - Registered UsersModule in AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: '.env',
     }),
     PrismaModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
